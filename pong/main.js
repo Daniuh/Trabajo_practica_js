c619eb327c8db5d508c67f343b4b44335979fc5b
Daniel Naranjo <naranjobenavidesd@gmail.com>
2022-02-11 11:16:19 -0500

    Se perfecciono el rebote de la pelota

--- pong/main.js
+++ pong/main.js
@@ -31,6 +31,9 @@
         this.speed_x = 3;
         this.board = board;
         this.direction = 1;
+        this.bounce_angle = 0;
+        this.max_bounce_angle = Math.PI / 12;
+        this.speed = 3;
 
         board.ball = this;
         this.kind = "circle";
@@ -41,8 +44,25 @@
             this.x += (this.speed_x * this.direction);
             this.y += (this.speed_y);
         },
+        get width(){
+            return this.radius * 2
+        },
+        get height(){
+            return this.radius * 2
+        },
         collision: function(bar){
             //Reacciona a la colisión con una barra que recibe como parametro
+            var relative_intersect_y = (bar.y + (bar.height / 2)) - this.y;
+
+            var normalized_intersect_y = relative_intersect_y / (bar.height / 2);
+
+            this.bounce_angle = normalized_intersect_y * this.max_bounce_angle;
+
+            this.speed_y = this.speed * -Math.sin(this.bounce_angle);
+            this.speed_x = this.speed * Math.cos(this.bounce_angle);
+
+            if(this.x > (this.board.width / 2)) this.direction = -1;
+            else this.direction = 1;
         }
     }
 }());
@@ -108,6 +128,7 @@
             if(this.board.playing){
                 this.clean();
                 this.draw();
+                this.check_collisions();
                 this.board.ball.move();
             }
         }
